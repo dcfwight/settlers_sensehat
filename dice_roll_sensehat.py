1ad4a913810d6cd55445382841dcf54132f61581
Doug Wight <dcfwight@gmail.com>
2021-02-12 15:27:43 +0000

    Start adding lists for drawing numbers and dice

--- dice_roll_sensehat.py
+++ dice_roll_sensehat.py
@@ -49,10 +49,52 @@ def report(roll, player):
 	print ('{} rolled {}. {} + {}'.format(player, d1+d2, d1, d2))
 	sense.clear()
 
+##############################################################
+# here we have a load of strings for showing numbers and dice #
+
+r = (255,0,0) # red
+g = (0,255,0) # green
+b = (0,0,255) # bluee
+n = (0,0,0)
+
+numbers = {
+	2: [
+	n,n,n,b,b,n,n,n,
+	n,n,b,n,b,n,n,n,
+	n,n,n,b,n,n,n,n,
+	n,n,b,b,b,n,n,n,
+	]
+}
+
+dice = {
+	1: [n,n,n,n,b,n,n,n,n],
+	2: [n,b,n,n,n,n,n,b,n],
+	3: [n,n,b,n,b,n,b,n,n],
+	4: [b,n,b,n,n,n,b,n,b],
+	5: [b,n,b,n,b,n,b,n,b],
+	6: [b,n,b,b,n,b,b,n,b]
+}
+
+def construct_dice_display(d1,d2):
+	"""Creates the bottom half of the sense hat display showing dice"""
+	display = []
+	display+=d1[:3]
+	display+=[n,n]
+	display+=d2[:3]
+	display+=d1[3:6]
+	display+=[n,n]
+	display+=d2[3:6]
+	display+=d1[6:9]
+	display+=[n,n]
+	display+=d2[6,9]
+	return display
+
 def test():
+	display = construct_dice_display(dice[1], dice[5])
+	print(display)
 	deck = create_full_deck(2)
 	deck = augment_deck(deck, n_remove = 10, n_add=10)
-	report(deck.pop(), 'Alice')
+	#report(deck.pop(), 'Alice')
 	
 def main():
 	pass
